da61f3f49598a154008ad962493f9d73e7c8820e
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-01-15 23:47:15 +0100

    Update demo.gif for multi-line

--- surround-funk.vim/example_use.py
+++ surround-funk.vim/example_use.py
@@ -1,13 +1,29 @@
 
     a = mean(data)
 
+
     b = more_data
+
 
     c = len(fancy)
 
-    d = np.mean(st.zscore(myarry), axis=1, dtype=np.float64)
 
-    e = st.bootstrap(other)
+    d = st.bootstrap(other)
+
+
+    e = np.mean(st.zscore(myarry), axis=1, dtype=np.float64)
+
+
+    f = st.bootstrap(np.mean(data),
+                        argA, argB)
+
+
+    g = np.mean(st.zscore(myarry),
+                axis=1, dtype=np.float64)
+
+
+
+
 
 
 
