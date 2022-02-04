d2e93818fcf18fda1cf5fd0cda6ac0601585319c
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-02-04 16:25:44 +0100

    Add example_use file

---
+++ vim-visual-history/example_use.c
@@ -0,0 +1,23 @@
+
+    a = mean(data)                               // 1st selection (char)
+
+
+    b = more_data                                // 2nd selection (line)
+
+
+    if answer == 42 {                            //
+        if bugs == 'bunny' {                     //
+            if injury == 'but a scratch' {       // 3th selection (block)
+                // do the thing                  //
+            }                                    //
+        }                                        //
+    }                                            //
+
+
+    if answer == 42 {                            // 6th selection
+        if bugs == 'bunny' {                     // 5th selection
+            if injury == 'but a scratch' {       // 4th selection (text object)
+                // do the thing                  // 4th selection
+            }                                    // 5th selection
+        }                                        // 6th selection
+    }
